f3b9c34c4b710ab144500b571bb0cbeb6e0a8640
KimTaver <kakashi4q@gmail.com>
2026-07-22 23:46:25 +0100

    Delete ready.js

--- ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client) => {
-  client.once("ready", () => {
-    console.log(`✅ ${client.user.tag} is online!`);
-    client.user.setActivity("!help | Taver Moderation");
-  });
-};
