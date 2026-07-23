f56311ed160bd1b41f0d15ccc32375a4b9b8a0e1
KimTaver <kakashi4q@gmail.com>
2026-07-23 11:43:36 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,8 @@
 module.exports = {
   name: "ping",
+  description: "Shows the bot latency",
 
-  execute(message, client) {
-    message.reply(`🏓 Pong! ${client.ws.ping}ms`);
+  execute(message, args, client) {
+    message.reply(`🏓 Pong! **${client.ws.ping}ms**`);
   },
 };
