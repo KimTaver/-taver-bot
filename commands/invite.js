14da6ff68db07d56d64ccc672b2ffe353dc37f43
KimTaver <kakashi4q@gmail.com>
2026-07-24 04:35:16 +0100

    Create commands/invite.js

---
+++ commands/invite.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require("discord.js");
+
+const CLIENT_ID = "YOUR_BOT_CLIENT_ID";
+
+module.exports = {
+  name: "invite",
+  description: "Get the Taver AI invite link",
+
+  async execute(message) {
+    const inviteLink = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&permissions=8&scope=bot%20applications.commands`;
+
+    const embed = new EmbedBuilder()
+      .setColor("#5865F2")
+      .setTitle("📨 Invite Taver AI")
+      .setDescription(
+        `Want Taver AI in your own server?\n\n**[Click here to invite Taver AI](${inviteLink})**`
+      )
+      .setFooter({ text: "Created by Kim_Taver" })
+      .setTimestamp();
+
+    message.reply({ embeds: [embed] });
+  },
+};
